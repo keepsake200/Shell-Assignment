79b020d8f358eed7ecfa53d371040a094181d752
Trevor Bakker <tbakker@gmail.com>
2021-08-20 23:14:16 -0500

    Updated GUID

--- msh.c
+++ msh.c
@@ -12,7 +12,7 @@
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 // 
-// 7f704d5f-9811-4b91-a918-57c1bb646b70
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
