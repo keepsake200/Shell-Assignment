b445055e2f84427cf02e9212892a4d616126af64
Ariel Zambeck <123191658+keepsake200@users.noreply.github.com>
2025-09-21 23:50:14 +0000

    msh.c

--- msh.c
+++ msh.c
@@ -1,18 +1,18 @@
 // The MIT License (MIT)
-// 
-// Copyright (c) 2016 Trevor Bakker 
-// 
+//
+// Copyright (c) 2016 Trevor Bakker
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// 
+//
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,42 +31,49 @@
 #include <string.h>
 #include <signal.h>
 
-#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
-                                // so we need to define what delimits our tokens.
-                                // In this case  white space
-                                // will separate the tokens on our command line
+#define WHITESPACE " \t\n" // We want to split our command line up into tokens
+                           // so we need to define what delimits our tokens.
+                           // In this case  white space
+                           // will separate the tokens on our command line
 
-#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+#define MAX_COMMAND_SIZE 255 // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports four arguments
+#define MAX_NUM_ARGUMENTS 11 // Mav shell supports 10 arguments 
 
 int main()
 {
 
-  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+  char *command_string = (char *)malloc(MAX_COMMAND_SIZE);
 
-  while( 1 )
+  while (1)
   {
     // Print out the msh prompt
-    printf ("msh> ");
+    printf("msh> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
     // This while command will wait here until the user
     // inputs something since fgets returns NULL when there
     // is no input
-    while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
+    while (!fgets(command_string, MAX_COMMAND_SIZE, stdin))
+      ;
 
     /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
-    int   token_count = 0;                                 
-                                                           
+    int i;
+    for( i = 0 ; i < MAX_NUM_ARGUMENTS; i++ )
+    {
+      token[i] = NULL;
+    }
+
+    int token_count = 0;
+
     // Pointer to point to the token
     // parsed by strsep
-    char *argument_ptr;                                         
-                                                           
-    char *working_string  = strdup( command_string );                
+    char *argument_ptr;
+
+    char *working_string = strdup(command_string);
 
     // we are going to move the working_string pointer so
     // keep track of its original value so we can deallocate
@@ -74,72 +81,104 @@ int main()
     char *head_ptr = working_string;
 
     // Tokenize the input strings with whitespace used as the delimiter
-    while ( ( (argument_ptr = strsep(&working_string, WHITESPACE ) ) != NULL) && 
-              (token_count<MAX_NUM_ARGUMENTS))
+    while (((argument_ptr = strsep(&working_string, WHITESPACE)) != NULL) &&
+           (token_count < MAX_NUM_ARGUMENTS))
     {
-      token[token_count] = strndup( argument_ptr, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
+      token[token_count] = strndup(argument_ptr, MAX_COMMAND_SIZE);
+      if (strlen(token[token_count]) == 0)
       {
         token[token_count] = NULL;
       }
-        token_count++;
-    }
+      token_count++;
+      // ! I don't think there should be a closing bracket here, but there was. I deleted it.
 
-    // Now print the tokenized input as a debug check
-    // Huh?
-    
-    // Part 3 If the user enters nothing, continue the prompt again 
-    if(token[0] == NULL) {
-      continue;
+    // GENERALLY NOT SURE IF THESE ARE WORKING RIGHT, LOOP END CONDITION CONCERNS 
+    // TODO: Search the token array for | and set a variable pipe_found to 1
+    for(int i = 0; i <MAX_NUM_ARGUMENTS; i++) {
+      if  (strcmp(token[i], " | ") == 0) {
+        int pipe_found = 1;
+        break;
+      } else {
+        i++;
+      }
     }
-    // Part 4 If the user enters quit or exit, exit with 0
-    else if(strcmp(token[0], "quit") == 0) {
-      exit(0);
-    }
-    else if (strcmp(token[0], "exit") == 0) {
-      exit(0);
-    }
-    // Part 9 handle cd
-    else if (strcmp(token[0], "cd") == 0) {
-      chdir(token[1]);
-    }
-    // Part 2 If the command is not supported your shell shall print the invalid command followed by “: Command not found.”
-    // Check for the 5 built ins, call exec, if it returns -1, print command found
-//     else if (strcmp(token[0], "exit") == 0 ||
-//     strcmp(token[0], "quit") == 0 ||
-//     strcmp(token[0], "history") == 0 ||
-//     strcmp(token[0], "!") == 0 ||
-//     strcmp(token[0], "fork") == 0 ||
-//     strcmp(token[0], "execvp") == 0) {
-    
-//     // Execvp needs more arguments?
-//     int check_validity = execvp(token[0]);
-//     if (check_validity == -1) {
-//         printf("%s, Command not found.\n", token[0]);
-//     }
-// }
+    // TODO: Search the token array for > and set a varaible redirect_found to 1
+     for(int i = 0; i <MAX_NUM_ARGUMENTS; i++) {
+      if (strcmp(token[i], " > ") == 0) {
+        int redirect_found = 1;
+        break;
+      }
+      else {
+        i++;
+      }
 
-    // Part 6 Your version of Mav shell shall support up to 10 command line parameters in addition to the command.
-    //if( argc > 11) {
-    //  printf("Too many arguments");
-    //  continue;
-    //}
-
-    // Part 8 Mav shell shall be implemented using fork(), wait() and one of the exec family of functions. Your Mav shell shall not use system(). Use of system() will result in a grade of 0.
-
-
-    // Cleanup allocated memory
-    for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
-    {
-      if( token[i] != NULL )
-      {
-        free( token[i] );
       }
     }
 
-    free( head_ptr );
+    // Part 3 If the user enters nothing, continue the prompt again
+    if (token[0] == NULL)
+    {
+      continue;
+    }
+    // Part 4 If the user enters quit or exit, exit with 0
+    else if (strcmp(token[0], "quit") == 0)
+    {
+      exit(0);
+    }
+    else if (strcmp(token[0], "exit") == 0)
+    {
+      exit(0);
+    }
+    // Part 9 handle cd
+    else if (strcmp(token[0], "cd") == 0)
+    {
+      chdir(token[1]);
+    }
+    // TODO Save last 50 commands and command line parameters
+    // TODO Part 11 You shall print the history log, excluding blank line entries when the user types history
+    else if (strcmp(token[0], "history" || "History") == 0) {
+    }
 
+    // TODO Part 12 The user can re-run any commnd in the history by typing !# where # is the number of the command to rerun.
+    else if (strcmp(token[0], "!#") == 0) {
+    }
+
+
+    else
+    {
+      // TODO handle |  if pipe_found == 1      ( pipe.c )
+      // TODO handle >  if redirect found == 1  ( pipe_redirect.c)
+      // ! ^^ Are these related to the loops I created earlier? and how? ^^
+      // else do the code below
+      pid_t pid = fork();
+      if (pid == 0)
+      {
+        int check_validity = execvp(token[0], &token[0]);
+        if (check_validity == -1)
+        {
+          printf("%s, Command not found.\n", token[0]);
+          exit(0);
+        }
+      }
+      else
+      {
+        int status;
+        wait(&status);
+      }
+    } 
+    // Part 16 Your program shall block the SIGINT and SIGTSTP signals. See sigint.c in Code-Examples
+
+    // Cleanup allocated memory
+    for (int i = 0; i < MAX_NUM_ARGUMENTS; i++)
+    {
+      if (token[i] != NULL)
+      {
+        free(token[i]);
+      }
+    }
+
+    free(head_ptr);
   }
-  return 0;
+  return 0; 
   // e2520ca2-76f3-90d6-0242ac120003
 }
